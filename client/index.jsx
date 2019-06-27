f00b7d11bcfefb1edfbef146a4d8d5b66a092366
yushi zhang <yushisusu@gmail.com>
2019-06-28 00:35:42 +1000

    Basic Structure

---
+++ client/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+	return (
+		<div>
+			<h1>Hello C Plus Lab</h1>
+		</div>
+	);
+};
+
+ReactDOM.render(<App />, document.querySelector('.react-container'));
