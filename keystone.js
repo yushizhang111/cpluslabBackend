67b60e0a6cc24fcb2acfd3f66905f62ad2cfdfc8
yushi zhang <yushisusu@gmail.com>
2019-06-29 16:49:49 +1000

    Add Merchandise and Setting models

--- keystone.js
+++ keystone.js
@@ -49,10 +49,12 @@ keystone.set('routes', require('./routes'));
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
-	posts: ['posts', 'post-categories'],
-	galleries: 'galleries',
-	enquiries: 'enquiries',
-	users: 'users',
+	Merchandises: ['Store', 'Merchandise', 'Item'],
+	// Stock: ['IncomingStock', 'OutcomingStock', 'Storage'],
+	Setting: ['Type'],
+	// Workshop: ['Workshop', 'Session'],
+	// Articles: ['Post', 'PostCategory', 'News'],
+	Admin: ['users'],
 });
 
 // Start Keystone to connect to your database and initialise the web server
