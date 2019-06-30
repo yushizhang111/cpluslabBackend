d08d9affc8f82c93f6dd5172fa8cd995241475c0
Zavier Xu <zavierxu250@gmail.com>
2019-06-30 16:13:03 +1000

    [IncommingStockModel] Build the incoming stock model and do modification.

--- models/Stock/IncomingStock.js
+++ models/Stock/IncomingStock.js
@@ -7,36 +7,45 @@ const Types = keystone.Field.Types;
  */
 
 const IncomingStock = new keystone.List('IncomingStock', {
+	map: { name: 'inId' },
 	autokey: {
-		from: 'name',
+		from: 'inId',
 		path: 'slug',
 		unique: true,
-		sortable: true,
+
 	},
+	track: true,
 });
 
 //  Tutor.defaultColumns
 
 IncomingStock.add({
-	name: { type: String, required: true },
-	chName: { type: String, label: '中文名' },
-	item: { type: String, ref: 'Item' },
-	store: { type: String, ref: 'Store' },
-	merchandiseURL: { type: String },
-	batch: { type: String },
-	color: { type: String },
-	size: { type: String },
-	pattern: { type: String },
-	Image: { type: Types.CloudinaryImage, autoCleanup: true, select: true },
-	Gallery: {
-		type: Types.CloudinaryImage,
-		autoCleanup: true,
-		select: true,
-		many: true,
-	},
-	description: { type: Types.Html, wysiwyg: true, height: 400 },
+	inId: { type: String, noedit: true, label: '入库ID (InID)' },
+	merchandise: { type: Types.Relationship, ref: 'Merchandise', label: '商品ID (Merchandises ID)' },
+	source: { type: String, label: '来源 (Source)' },
+	orderId: { type: String, label: '下单ID (Order ID)' },
+	trackingId: { type: String, label: '跟踪单号 (Tracking ID)' },
+	shipping: { type: String, label: '物流公司 (Shipping Company)' },
+	status: { type: String, label: '物流状态 (Status)' },
+	shippingprice: { type: Number, label: '运费 (Shipping price)' },
+	amount: { type: Number, label: '数量 (Amount)' },
+	purchasingcost: { type: Number, label: '买入价格PurchasingCost(AUD)' },
+	intime: { type: Types.Date, label: '入库时间 (Instorage Time)' },
+
 });
 
+function generateID () {
+	const date = Date.now().toString();
+	console.log(date);
+	return 'IN' + date.slice(-6);
+}
+
+IncomingStock.schema.pre('save', function (next) {
+
+	this.inId = generateID();
+	console.log(this.inId);
+	next();
+});
 // City.relationship({ ref: 'Tutor', refPath: 'city', path: 'tutor' });
 // City.relationship({ ref: 'University', refPath: 'location', path: 'university' });
 // City.relationship({ ref: 'Job', refPath: 'city', path: 'job' });
